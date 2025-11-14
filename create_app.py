4b80b80ead9a89fd97635c7fa11c3b8dde313405
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-15 00:16:14 +0100

    Create create_app.py

---
+++ create_app.py
@@ -0,0 +1,9 @@
+      -w 10 \
+      -k gevent \
+      --worker-connections 1000 \
+      --timeout 120 \
+      -b  0.0.0.0:6666 \
+      --limit-request-line 0 \
+      --limit-request-field_size 0 \
+      --statsd-host localhost:8125 \
+      "superset.app:create_app()"
