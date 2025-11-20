f8a9ec153f0aaf14a148e2bf2f9ea660ab6494bc
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-20 09:10:01 +0100

    Update main.dart

--- main.dart
+++ main.dart
@@ -5,3 +5,25 @@ void main() {
     ),
   );
 }
+class SomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Retrieve the `viewId` where this Widget is being built:
+    final int viewId = View.of(context).viewId;
+    // ...
+  
+    MultiViewApp(
+  viewBuilder: (BuildContext context) {
+    // Retrieve the `viewId` where this Widget is being built:
+    final int viewId = View.of(context).viewId;
+    // Decide what to render based on `viewId`...
+  },
+)
+      // Adding a view with initial data...
+let viewId = app.addView({
+  hostElement: someElement,
+  initialData: {
+    greeting: 'Hello, world!',
+    randomValue: Math.floor(Math.random() * 100),
+  }
+});
